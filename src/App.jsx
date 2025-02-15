2481eff4f3f89898753bd3fd93da26d650bd76f2
Abdulislam Asif <abdusslamasif8955@gmail.com>
2025-02-15 16:54:58 +0500

    Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Body from "./components/Body/Body";
+import Login from "./components/Login/Login";
+import Profile from "./components/Profile/Profile";
 const App = () => {
   return (
     <>
-    Hello World
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
