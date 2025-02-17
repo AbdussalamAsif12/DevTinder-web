09a306892a2f9c9f6acba4525a9158389f996d90
Abdulislam Asif <abdusslamasif8955@gmail.com>
2025-02-17 15:02:49 +0500

    setup redux store

---
+++ src/components/Feed/Feed.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Feed = () => {
+  return (
+    <>
+      <h1>User's Feed</h1>
+    </>
+  );
+};
+
+export default Feed;
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
-
+import axios from "axios";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../../utils/constant";
 const Login = () => {
-  return (
-    <>Login</>
-  )
-}
+  const [emailId, setEmail] = useState("osama@gmail.com");
+  const [password, setPassword] = useState("Node@123js");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-export default Login
+  const HandleLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/login",
+        { emailId, password },
+        {
+          withCredentials: true,
+        }
+      );
+
+      dispatch(addUser(res.data));
+      return navigate("/feed");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  return (
+    <>
+      <div className="flex justify-center my-10">
+        <div className="card bg-base-300 w-96 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title justify-center">Login</h2>
+            <div>
+              <label className="form-control w-full max-w-xs py-4">
+                <div className="label">
+                  <span className="label-text">Email Id</span>
+                </div>
+                <input
+                  type="text"
+                  placeholder="Type here"
+                  className="input input-bordered w-full max-w-xs"
+                  value={emailId}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </label>
+              <label className="form-control w-full max-w-xs my-2">
+                <div className="label">
+                  <span className="label-text">Password</span>
+                </div>
+                <input
+                  type="text"
+                  placeholder="Type here"
+                  className="input input-bordered w-full max-w-xs"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </label>
+            </div>
+            <div className="card-actions justify-center">
+              <button className="btn btn-primary" onClick={HandleLogin}>
+                Login
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
