c499049fa18dfef38616d13b8a846a73ba0f24a1
Abdulislam Asif <abdusslamasif8955@gmail.com>
2025-02-19 13:50:26 +0500

    redirect to login if user is not authenticated

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import axios from "axios";
 import Navbar from "../Navbar/Navbar";
-import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
+import { Outlet } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../../utils/userSlice";
+import { useEffect } from "react";
+import { BASE_URL } from "../../utils/constant";
+import { useNavigate } from "react-router-dom";
 const Body = () => {
+  const dispatch = useDispatch();
+  const Navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status == 401) {
+        Navigate("/login");
+      }
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return (
     <>
       <Navbar />
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
   const user = useSelector((store) => store.user);
   console.log(user);
@@ -8,7 +8,7 @@ const Navbar = () => {
     <>
       <div className="navbar bg-base-300">
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">DevTinder</a>
+          <Link to="/feed" className="btn btn-ghost text-xl">DevTinder</Link>
         </div>
         <div className="flex-none gap-2">
           {user && (
@@ -28,10 +28,10 @@ const Navbar = () => {
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
               >
                 <li>
-                  <a className="justify-between">
+                  <Link to="/profile" className="justify-between">
                     Profile
                     <span className="badge">New</span>
-                  </a>
+                  </Link>
                 </li>
                 <li>
                   <a>Settings</a>
