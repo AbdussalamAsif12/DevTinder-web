2dfd352a87d97f2d79a88766ce37ea6dfc0ddaba
Abdulislam Asif <abdusslamasif8955@gmail.com>
2025-02-22 13:47:54 +0500

    feture: Connection page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Profile from "./components/Profile/Profile";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 import Feed from "./components/Feed/Feed";
+import Connections from "./components/Connections/Connections";
+import Request from "./components/Request/Request";
 
 const App = () => {
   return (
@@ -17,6 +19,8 @@ const App = () => {
               <Route path="/feed" element={<Feed />}></Route>
               <Route path="/login" element={<Login />}></Route>
               <Route path="/profile" element={<Profile />}></Route>
+              <Route path="/connections" element={<Connections />}></Route>
+              <Route path="/requests" element={<Request />}></Route>
             </Route>
           </Routes>
         </BrowserRouter>
--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import feedSlice from "./feedSlice";
+import connectionReducer from "./connectionSlice"
 const appStore = configureStore({
   reducer: {
     user: userReducer,
     feed: feedSlice,
+    connections:connectionReducer
   },
 });
 
